ecae1bd947237bdd68584be5094c17ba5c5dd512
RADA <rada@RADAs-MacBook-Air.local>
2022-11-10 10:56:12 +0700

    feat: add about&news&service&portofolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
         '5': '5 5 0%',
         '6': '6 6 0%'
         
+      },
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+      
+        widest: '.4.5em',
       }
     },
   },
